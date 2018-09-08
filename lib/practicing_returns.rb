5948f696bf18173249d2185a17c913af05064b19
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 15:59:45 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,21 +1,10 @@
 require 'pry'
 
 def hello(array)
-  if array != []
-    i = 0
-    collection = []
-    while i < array.length
-      yield(make_sentence(array, i))
-      collection << object
-      i += 1
-    end
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
   end
-  collection
 end
-
-def make_sentence(array, int)
-  "This, #{array[int]}, is a sentence.}"
-end
-
-names = ["Tim", "Tom", "Jim"]
-hello(names)
